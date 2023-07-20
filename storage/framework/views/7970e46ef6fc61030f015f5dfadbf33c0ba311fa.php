9038d34aaa2c4ac8699746e83b5deb675e4a31fb
byyll <exsample015@gmail.com>
2023-07-20 14:42:18 +0700

    byyll

---
+++ storage/framework/views/7970e46ef6fc61030f015f5dfadbf33c0ba311fa.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+     ©  <?php echo e(date('Y')); ?> Simple Absen Project <span class="d-none d-sm-inline-block"> - Project <i class="mdi mdi-emoticon-cool text-danger"></i> by Maftuhul Nabil</span>.
+</footer><?php /**PATH D:\AttendanceMS-Laravel\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
